d51ab617593e4814d0c52ad09429f200158caa6b
Gift Mahlangu <giftnkz6@gmail.com>
2022-11-19 23:39:00 +0200

    Added style

--- js/code.js
+++ js/code.js
@@ -1,7 +1,20 @@
 let search = document.getElementById("search")
-let char = document.getElementById("word")
+let char = document.getElementById("char")
 
-search.addEventListener('click', () =>{
-    let searched = document.getElementById("search").value;
-    document.write(`Value was found at position: ${searched}`);
+search.addEventListener("click", (e) =>{
+    e.preventDefault();
+    let searched = document.getElementById("sentence").value;
+
+    // document.write(`Number of chars: ${searched.length}`)
+    console.log(`Number of chars: ${searched.length}`);
+    
+});
+
+char.addEventListener('click', (e) => {
+    e.preventDefault();
+    search= document.getElementById("sentence").value;
+    let numberOfChar = document.getElementById("word").value;
+    
+    // document.write(`${numberOfChar} is at position: ${search.indexOf(numberOfChar)}`);
+    console.log(`${numberOfChar} is at position: ${search.indexOf(numberOfChar)}`);
 });
