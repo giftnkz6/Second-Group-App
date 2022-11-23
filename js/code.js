fc5e827ff12ba915314970f556d1c81cb24e659e
giftnkz6 <giftnkz6@gmail.com>
2022-11-23 08:59:23 +0200

    Arranged the buttons inline

--- js/code.js
+++ js/code.js
@@ -4,8 +4,6 @@ let char = document.getElementById("char")
 search.addEventListener("click", (e) =>{
     e.preventDefault();
     let searched = document.getElementById("sentence").value;
-
-    // document.write(`Number of chars: ${searched.length}`)
     console.log(`Number of chars: ${searched.length}`);
     document.getElementById("nSentence").innerHTML = `Number of chars: ${searched.length}`
     
@@ -15,8 +13,5 @@ char.addEventListener('click', (e) => {
     e.preventDefault();
     search= document.getElementById("sentence").value;
     let numberOfChar = document.getElementById("word").value;
-    
-    // document.write(`${numberOfChar} is at position: ${search.indexOf(numberOfChar)}`);
-    // console.log(`${numberOfChar} is at position: ${search.indexOf(numberOfChar)}`);
     document.getElementById("nChars").innerHTML = `${numberOfChar} is at position: ${search.indexOf(numberOfChar)}`
 });
